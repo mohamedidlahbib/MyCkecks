8c0eed9a845b98059287a3124f1979bf22a5dd65
med <muhaid35@gmail.com>
2023-05-02 11:51:35 +0100

    :second commit

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/home',[HomeController::class,'redirect']);
+Route::get('/',[HomeController::class,'index']);
 
 
 
